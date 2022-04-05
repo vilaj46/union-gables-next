72f11e4345dfa54c52c411eda9bcbbfa2b41b2fe
Julian <vilaj46@gmail.com>
2022-04-05 17:59:49 -0400

    Fixe spacing on RedBallLinks.

--- components/RedBallLinks/index.js
+++ components/RedBallLinks/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Container } from "@mui/material";
 import styled from "styled-components";
 
 import divideLinksEvenly from "./utilities/divideLinksEvenly";
@@ -11,12 +12,18 @@ import getRandomKey from "../../clientUtils/getRandomKey";
 
 import sampleData from "./data";
 
-const Container = styled.ul`
+const LinksContainer = styled.ul`
   display: flex;
+  flex-direction: column;
   list-style-type: none;
   justify-content: space-around;
   text-align: center;
   margin-bottom: 3.5rem;
+  padding: 0;
+
+  @media (min-width: 600px) {
+    flex-direction: row;
+  }
 `;
 
 function RedBallLinks({ links = sampleData, randomKey = undefined }) {
@@ -24,11 +31,13 @@ function RedBallLinks({ links = sampleData, randomKey = undefined }) {
   if (links.length > 0) {
     return (
       <Container>
-        {splitLinks.map((split) => {
-          return (
-            <RedLinkColumn links={split} key={randomKey || getRandomKey()} />
-          );
-        })}
+        <LinksContainer>
+          {splitLinks.map((split) => {
+            return (
+              <RedLinkColumn links={split} key={randomKey || getRandomKey()} />
+            );
+          })}
+        </LinksContainer>
       </Container>
     );
   } else {
